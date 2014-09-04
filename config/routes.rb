6168573834b8d04bd9ac5ced6966b621881c2df1
Nathanial L. McConnell <FluxAugur@gmail.com>
2014-09-04 12:29:44 -0400

    redirecting signed-in user to task list page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  resources :task_lists, only: [:show]
+
   namespace :api, defaults: { format: :json } do
     resources :task_lists, only: [:index] do
       resources :tasks, only: [:index, :create, :update, :destroy]
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,12 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, :type => :controller do
+  let(:user) { create(:user) }
 
   describe "GET index" do
-    it "returns http success" do
+    it "should render when user is not signed in" do
       get :index
-      expect(response).to be_success
+      response.should be_ok
+      response.should render_template('index')
+    end
+
+    it "should redirect to the user's task list if signed in" do
+      sign_in(user)
+      get :index
+      response.should redirect_to(task_list_path(id: user.task_list))
     end
   end
-
 end
