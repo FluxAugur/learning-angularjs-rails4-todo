b193e3df8fb70e0f4195e0d47c63eb89dee98c24
Nathanial L. McConnell <FluxAugur@gmail.com>
2014-09-04 13:52:33 -0400

    preventing users from seeing other user's lists

--- spec/controllers/api/task_lists_controller_spec.rb
+++ spec/controllers/api/task_lists_controller_spec.rb
@@ -1,17 +1,28 @@
 require 'rails_helper'
 
 describe TaskListsController do
-  describe "GET show" do
-    it "should return 200 for existing task list" do
-      task_list = create(:task_list)
-      get :show, id: task_list.id
-      response.should be_ok
-    end
+  context "for a logged-in user with a task list" do
+    let(:task_list) { create(:task_list) }
+    let(:user) { task_list.owner }
+    before { sign_in(user) }
 
-    it "should return 404 when task list doesn't exist" do
-      expect {
-        get :show, id: 1
-      }.to raise_error(ActiveRecord::RecordNotFound)
+    describe "GET show" do
+      it "should return 200 for the task list" do
+        get :show, id: task_list.id
+        response.should be_ok
+      end
+
+      it "should raise RecordNotFound when task list doesn't exist" do
+        expect {
+          get :show, id: 1
+        }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+
+      it "should return 404 when task list doesn't exist" do
+        expect {
+          get :show, id: 1
+        }.to raise_error(ActiveRecord::RecordNotFound)
+      end
     end
   end
 end
